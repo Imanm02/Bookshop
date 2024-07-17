619fba0e1bc02bed3d2e82e4a33ba7b5d125c141
Imanm02 <imanm1381@gmail.com>
2024-07-17 20:29:02 +0330

    Implement Secure Hash Function

--- Bakcned/bookshop/bookshop/shop/utils/hash.py
+++ Bakcned/bookshop/bookshop/shop/utils/hash.py
@@ -1,2 +1,10 @@
-def hash(input):
-    return "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4"
+import hashlib
+import os
+
+def hash(password):
+    """Create a SHA-256 hash with a random salt."""
+    salt = os.urandom(32)  # Generates a 32 byte salt
+    pwd_hash = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+    salt_hex = salt.hex()
+    hash_hex = pwd_hash.hex()
+    return f"{salt_hex}${hash_hex}"
